1e61843e25b49be31076fe1654c794328db7bb56
ts-3156 <ts_3156@yahoo.co.jp>
2018-08-03 01:57:48 +0900

    Add summary column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408101440) do
+ActiveRecord::Schema.define(version: 2018_04_08_101440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
+    t.text "summary", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
