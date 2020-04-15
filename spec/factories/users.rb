378b7c5e7f3db867105272c29af44df7af893cc0
ts-3156 <ts_3156@yahoo.co.jp>
2020-04-15 19:17:52 +0900

    Update factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "person-#{n}@example.com" }
+    sequence(:email) { |n| "person-#{n}@example.com" }
     email
-    password '123456'
+    password { '123456' }
   end
 end
